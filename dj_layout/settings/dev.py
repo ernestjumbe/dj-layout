7238f1069ddd82dcb4d092129526b1f935c49650
Ernest <e2jeyy@gmail.com>
2013-06-25 01:38:05 +0200

    Added developments settings file.

---
+++ dj_layout/settings/dev.py
@@ -0,0 +1,20 @@
+"""Settings for Development Server"""
+from {{ project_name }}.settings.base import *
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+VAR_ROOT = '/var/www/{{ project_name }}'
+MEDIA_ROOT = os.path.join(VAR_ROOT, 'uploads')
+STATIC_ROOT = os.path.join(VAR_ROOT, 'static')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '{{ project_name }}',
+#        'USER': 'dbuser',
+#        'PASSWORD': 'dbpassword',
+    }
+}
+
+# WSGI_APPLICATION = '{{ project_name }}.wsgi.dev.application'
