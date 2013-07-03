dd5998176adeca07bd942046fcbc01597446f1b5
Ernest <e2jeyy@gmail.com>
2013-07-03 15:31:44 +0200

    Updated root urls configuration.

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,17 +1,17 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.conf.urls.defaults import patterns, url, include
+from django.conf import settings
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'dj_layout.views.home', name='home'),
-    # url(r'^dj_layout/', include('dj_layout.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+   # (r'', include('{{ project_name }}.apps.')),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG and settings.MEDIA_ROOT:
+    urlpatterns += static(settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT)
