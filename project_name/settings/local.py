e935023a662abc6c1225ef77cb42de0e9066ec3e
Ernest <e2jeyy@gmail.com>
2013-07-05 03:05:04 +0200

    Added debugg toolbar setins and removed redundant path calculations.

--- project_name/settings/local.py
+++ project_name/settings/local.py
@@ -28,37 +28,37 @@ DATABASES = {
     }
 }
 
-#==============================================================================
-# Calculation of directories relative to the project module location
-#==============================================================================
-
-media_dir = os.path.join(BASE_PATH, 'media')
-
-if not os.path.exists(media_dir):
-	os.mkdir(media_dir)
-
-#==============================================================================
-# Project URLS and media settings
-#==============================================================================
-
-STATIC_ROOT = os.path.join(media_dir, 'static')
-MEDIA_ROOT = os.path.join(media_dir, 'uploads')
-
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_DIR, 'static'),
-)
-
-#==============================================================================
-# Templates
-#==============================================================================
-
-TEMPLATE_DIRS = (
-    os.path.join(BASE_PATH, 'templates'),
-)
-
-TEMPLATE_CONTEXT_PROCESSORS += (
+INSTALLED_APPS += (
+    'debug_toolbar',
+  
 )
 
 #==============================================================================
 # Miscellaneous local settings
 #==============================================================================
+
+#Debug toolbar settings
+#----------------------
+
+INTERNAL_IPS = ('127.0.0.1',)
+
+
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+)
+
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+}
+
+def show_toolbar(request):
+    return True
+SHOW_TOOLBAR_CALLBACK = show_toolbar
