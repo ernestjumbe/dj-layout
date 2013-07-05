817f6560f2254136543735e0de3b7734eed17e1c
Ernest <e2jeyy@gmail.com>
2013-07-05 02:55:03 +0200

    Recalculated paths to media directories.

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -54,24 +54,10 @@ import {{ project_name }} as project_module
 PROJECT_DIR = os.path.dirname(os.path.realpath(project_module.__file__))
 BASE_PATH     = os.path.dirname(PROJECT_DIR)
 
-PYTHON_BIN = os.path.dirname(sys.executable)
-ve_path = os.path.dirname(os.path.dirname(os.path.dirname(PROJECT_DIR)))
-# Assume that the presence of 'activate_this.py' in the python bin/
-# directory means that we're running in a virtual environment.
-if os.path.exists(os.path.join(PYTHON_BIN, 'activate_this.py')):
-    # We're running with a virtualenv python executable.
-    VAR_ROOT = os.path.join(os.path.dirname(PYTHON_BIN), 'var')
-elif ve_path and os.path.exists(os.path.join(ve_path, 'bin',
-        'activate_this.py')):
-    # We're running in [virtualenv_root]/src/[project_name].
-    VAR_ROOT = os.path.join(ve_path, 'var')
-else:
-    # Set the variable root to a path in the project which is
-    # ignored by the repository.
-    VAR_ROOT = os.path.join(PROJECT_DIR, 'var')
+media_dir = os.path.join(BASE_PATH, 'media')
 
-if not os.path.exists(VAR_ROOT):
-    os.mkdir(VAR_ROOT)
+if not os.path.exists(media_dir):
+	os.mkdir(media_dir)
 
 #==============================================================================
 # Project URLS and media settings
@@ -89,9 +75,9 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/uploads/'
 
-STATIC_ROOT = os.path.join(VAR_ROOT, 'static')
+STATIC_ROOT = os.path.join(media_dir, 'static')
 
-MEDIA_ROOT = os.path.join(VAR_ROOT, 'uploads')
+MEDIA_ROOT = os.path.join(media_dir, 'uploads')
 
 STATICFILES_DIRS = (
     os.path.join(PROJECT_DIR, 'static'),
