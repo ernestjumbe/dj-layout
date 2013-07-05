28db96162649cb5ebb0d67913255437578713848
Ernest <e2jeyy@gmail.com>
2013-07-05 02:45:20 +0200

    Commented and refactoored base settings.

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -1,18 +1,22 @@
 # Django settings for dj_layout project.
-from django.conf.global_settings import * 
+from django.conf.global_settings import *
+
+#==============================================================================
+# Generic Django project settings
+#============================================================================== 
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
-)
-
-MANAGERS = ADMINS
-
+SITE_ID = 1
 
 TIME_ZONE = 'Europe/Copenhagen'
 
+USE_TZ = True
+
+USE_I18N = True
+
+USE_L10N = True
 
 LANGUAGE_CODE = 'en-gb'
 
@@ -20,67 +24,8 @@ LANGUAGES = (
     ('en', 'English'),
 )
 
-SITE_ID = 1
-
-
-USE_I18N = True
-
-
-USE_L10N = True
-
-USE_TZ = True
-
-
-MEDIA_ROOT = ''
-
-
-MEDIA_URL = ''
-
-
-STATIC_ROOT = ''
-
-
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
-
-
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
-
-
 SECRET_KEY = '{{ secret_key }}'
 
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
-
-ROOT_URLCONF = '{{ project_name }}.urls'
-
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -93,31 +38,98 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    # 'apps.',
     
     #'south',
 )
 
+#==============================================================================
+# Calculation of directories relative to the project module location
+#==============================================================================
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
+import os
+import sys
+import {{ project_name }} as project_module
+
+PROJECT_DIR = os.path.dirname(os.path.realpath(project_module.__file__))
+BASE_PATH     = os.path.dirname(PROJECT_DIR)
+
+PYTHON_BIN = os.path.dirname(sys.executable)
+ve_path = os.path.dirname(os.path.dirname(os.path.dirname(PROJECT_DIR)))
+# Assume that the presence of 'activate_this.py' in the python bin/
+# directory means that we're running in a virtual environment.
+if os.path.exists(os.path.join(PYTHON_BIN, 'activate_this.py')):
+    # We're running with a virtualenv python executable.
+    VAR_ROOT = os.path.join(os.path.dirname(PYTHON_BIN), 'var')
+elif ve_path and os.path.exists(os.path.join(ve_path, 'bin',
+        'activate_this.py')):
+    # We're running in [virtualenv_root]/src/[project_name].
+    VAR_ROOT = os.path.join(ve_path, 'var')
+else:
+    # Set the variable root to a path in the project which is
+    # ignored by the repository.
+    VAR_ROOT = os.path.join(PROJECT_DIR, 'var')
+
+if not os.path.exists(VAR_ROOT):
+    os.mkdir(VAR_ROOT)
+
+#==============================================================================
+# Project URLS and media settings
+#==============================================================================
+
+ROOT_URLCONF = '{{ project_name }}.urls'
+
+LOGIN_URL = '/login/'
+
+LOGOUT_URL = '/logout/'
+
+LOGIN_REDIRECT_URL = '/'
+
+STATIC_URL = '/static/'
+
+MEDIA_URL = ''
+
+STATIC_ROOT = ''
+
+MEDIA_ROOT = ''
+
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+#==============================================================================
+# Templates
+#==============================================================================
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+TEMPLATE_CONTEXT_PROCESSORS += (
+)
+
+#==============================================================================
+# Middleware
+#==============================================================================
+
+MIDDLEWARE_CLASSES += (
+)
+
+#==============================================================================
+# Auth / security
+#==============================================================================
+
+AUTHENTICATION_BACKENDS += (
+)
+
+#==============================================================================
+# Miscellaneous project settings
+#==============================================================================
+
+#==============================================================================
+# Third party app settings
+#==============================================================================
