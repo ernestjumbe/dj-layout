59baae27eaf6b5c91fb867a81d34fe91b558fbc3
Ernest <e2jeyy@gmail.com>
2013-07-12 02:46:31 +0200

    Refactored project urls.

--- project_name/urls.py
+++ project_name/urls.py
@@ -7,8 +7,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-   # (r'', include('{{ project_name }}.apps.')),
-    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # Project urls here.
+    # url(r'^', include('appname.urls')),
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
 )
 
