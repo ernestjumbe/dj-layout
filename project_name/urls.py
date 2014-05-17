933c62939a34c039770cd82f16872a67f249dbd4
ernestjumbe <e2jeyy@gmail.com>
2014-05-18 01:29:02 +0200

    Added index url

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.static import static
 from django.conf.urls import patterns, url, include
 from django.conf import settings
-
+from django.views.generic.base import TemplateView
 from django.contrib import admin
 
 admin.autodiscover()
@@ -10,6 +10,7 @@ urlpatterns = patterns('',
     # Project urls here.
     # url(r'^', include('appname.urls')),
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^$', TemplateView.as_view(template_name="index.html"), {}, name='index'),
     (r'^admin/', include(admin.site.urls)),
 )
 
